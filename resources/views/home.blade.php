ccd29b5e7ccd918a515066f171f44bcf70aba55a
ismailtgr <ismailtegar1@gmail.com>
2025-11-26 14:40:54 +0700

    homepage fixx

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,15 +34,15 @@
                 </form>
 
                 <div class="flex gap-6">
-                    <a href="#" class="flex items-center gap-2 text-gray-600 hover:text-blue-600 transition">
+                    <a href="{{ route('register') }}" class="flex items-center gap-2 text-gray-600 hover:text-blue-600 transition">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
                         </svg>
-                        <span>Keranjang</span>
+                        <span>Register</span>
                     </a>
-                    <a href="#" class="flex items-center gap-2 text-gray-600 hover:text-blue-600 transition">
+                    <a href="{{ route('login') }}" class="flex items-center gap-2 text-gray-600 hover:text-blue-600 transition">
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
                         </svg>
                         <span>Login</span>
                     </a>
